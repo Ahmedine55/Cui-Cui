4aa0138d52a565ac731b6a183e3da0da4556265d
Ahmed IDRISS Mohamed <ahmed.idriss-mohamed@pictime-groupe.com>
2018-07-02 14:56:09 +0200

    merge request feat(security)

--- src/main/java/ifi/jee/CuiCui/security/WebSecurity.java
+++ src/main/java/ifi/jee/CuiCui/security/WebSecurity.java
@@ -13,7 +13,7 @@ public class WebSecurity {
   IUserMetier iUserMetier;
 
     public boolean checkUserId(Authentication auth) {
-        System.out.println(" == "+auth.getPrincipal().toString());
+        //System.out.println(" == "+auth.getPrincipal().toString());
         //String username = ((User) auth.getPrincipal()).toString();
         //User result = iUserMetier.findUserByUsername(username);
         //System.out.println(result.getId()+" == "+username);
